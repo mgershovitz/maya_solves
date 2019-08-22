3ff646193572cc5befa9ae1be976bb294f1887be
Maya Gershovitz <maya@singular.net>
2019-08-22 23:57:46 +0300

    Update basic_calc.py
    using costs operators instead of strings

--- calculator/basic_calc.py
+++ calculator/basic_calc.py
@@ -1,32 +1,44 @@
 from operator import add, truediv, imul, isub
 
 
+class OpStr(object):
+    ADD = "+"
+    SUB = "-"
+    MUL = "*"
+    DIV = "/"
+    PAR_L = "("
+    PAR_R = ")"
+
 class BasicCalculator(object):
     def __init__(self):
         self.operator_to_action = {
-            "+": add,
-            "-": isub,
-            "*": imul,
-            "/": truediv
+            OpStr.ADD: add,
+            OpStr.SUB: isub,
+            OpStr.MUL: imul,
+            OpStr.DIV: truediv
         }
 
     def __eval_expression(self, arg_1, operator, arg_2):
         action = self.operator_to_action[operator]
         return action(arg_1, arg_2)
 
+    def __evaluate_next_val_and_get_length(self, exp_arr, index):
+        sub_exp_length = 2
+        val = exp_arr[index + 1]
+        if val == OpStr.PAR_L:
+            sub_exp_length, val = self.__solve_expression_wrap(exp_arr[index + 2:])
+        return sub_exp_length, val
+
     def __solve_expression(self, exp_arr, current_operators):
         new_exp = [exp_arr[0]]
         i = 1
         while i < len(exp_arr):
             operator = exp_arr[i]
-            if operator == ")":
+            if operator == OpStr.PAR_R:
                 current_exp_length = i + 2
                 return current_exp_length, new_exp
 
-            sub_exp_length = 2
-            val = exp_arr[i + 1]
-            if val == "(":
-                sub_exp_length, val = self.__solve_expression_wrap(exp_arr[i + 2:])
+            sub_exp_length, val = self.__evaluate_next_val_and_get_length(exp_arr, i)
 
             if operator in current_operators:
                 res = self.__eval_expression(new_exp.pop(), operator, val)
@@ -36,11 +48,12 @@ class BasicCalculator(object):
                 new_exp.append(val)
 
             i += sub_exp_length
+
         return len(exp_arr), new_exp
 
     def __solve_expression_wrap(self, exp_arr):
-        sub_exp_length, exp_arr = self.__solve_expression(exp_arr, ["*", "/"])
-        _, exp_arr = self.__solve_expression(exp_arr, ["+", "-"])
+        sub_exp_length, exp_arr = self.__solve_expression(exp_arr, [OpStr.MUL, OpStr.DIV])
+        _, exp_arr = self.__solve_expression(exp_arr, [OpStr.ADD, OpStr.SUB])
         return sub_exp_length + 1, exp_arr[0]
 
     def solve(self, exp_arr):
