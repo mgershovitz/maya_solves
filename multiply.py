2ce19d88b5dd0f9400adecc1a4d7cfe91db538cd
Maya Gershovitz <mgershovitz@gmail.com>
2021-11-09 21:33:33 +0200

    Add multiplication fraction test

--- multiply.py
+++ multiply.py
@@ -9,6 +9,7 @@ def run_tests():
     assert multiply(0,5) == 0
     assert multiply(1,5) == 5
     assert multiply(-5,5) == -25
+    assert multiply(0.5,5) == 2.5
 
 if __name__ == '__main__':
     run_tests()
